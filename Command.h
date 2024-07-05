acffb0541cba4384fffda6e6bba7c2a8c4dc22e1
kimkyungjae1112 <rudwo4861@naver.com>
2024-07-05 17:39:04 +0900

    명령 패턴

--- Command.h
+++ Command.h
@@ -1,8 +1,16 @@
 #pragma once
 #ifndef __COMMAND_H__
 
-namespace ActorCommand
+namespace HActorCommand
 {
+	enum class InputButton
+	{
+		BUTTON_X,
+		BUTTON_Y,
+		BUTTON_A,
+		BUTTON_B
+	};
+
 	class GameActor
 	{
 	public:
@@ -42,9 +50,69 @@ namespace ActorCommand
 	public:
 		virtual void Execute(GameActor& Actor) { Actor.LurchInEffectively(); }
 	};
+
+	class InputHandler
+	{
+	public:
+		InputHandler();
+
+		Command* HandleInput();
+
+		void SetInputButton(InputButton button);
+
+
+	private:
+		Command* ButtonX;
+		Command* ButtonY;
+		Command* ButtonA;
+		Command* ButtonB;
+
+		InputButton CurrentButton;
+	};
+
+	InputHandler::InputHandler() : CurrentButton(InputButton::BUTTON_X)
+	{
+		ButtonX = new JumpCommand;
+		ButtonY = new FireCommand;
+		ButtonA = new SwapWeaponCommand;
+		ButtonB = new Lurch;
+	}
+
+	Command* InputHandler::HandleInput()
+	{
+		switch (CurrentButton)
+		{
+		case InputButton::BUTTON_X:
+			return ButtonX;
+		case InputButton::BUTTON_Y:
+			return ButtonY;
+		case InputButton::BUTTON_A:
+			return ButtonA;
+		case InputButton::BUTTON_B:
+			return ButtonB;
+		default:
+			return nullptr;
+		}
+	}
+
+	void InputHandler::SetInputButton(InputButton button)
+	{
+		CurrentButton = button;
+	}
+
+	void ApplyActor(GameActor& Actor, InputButton button)
+	{
+		InputHandler inputHandler;
+		inputHandler.SetInputButton(button);
+		Command* command = inputHandler.HandleInput();
+		if (command)
+		{
+			command->Execute(Actor);
+		}
+	}
 }
 
-namespace UndoRedoCommand
+namespace HUndoRedoCommand
 {
 	class Unit
 	{
